994e86ac296bf39b25737e6a482c6a8f0b73a974
58871 <58871@etu.he2b.be>
2024-09-26 09:50:02 +0200

    fin test unitaires

--- attendance-app/app/Models/Student.php
+++ attendance-app/app/Models/Student.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'matriculation_number'];
 }
--- attendance-app/tests/Unit/StudentTest.php
+++ attendance-app/tests/Unit/StudentTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace Tests\Unit;
 
@@ -13,23 +13,24 @@ class StudentTest extends TestCase
     /** @test */
     public function it_can_add_a_student()
     {
-        $student = Student::create(['number' => 1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
-        $this->assertDatabaseHas('students', ['number' => 1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
+        $student = Student::create(['name' => 'SpongeBob SquarePants', 'matriculation_number' => '1']);
+        $this->assertDatabaseHas('students', ['matriculation_number' => '1', 'name' => 'SpongeBob SquarePants']);
     }
 
     /** @test */
     public function it_throws_exception_when_number_is_negative()
     {
-        $this->expectException(\Exception::class);
-        Student::create(['number' => -1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
+     $this->expectException(\Exception::class);
+     Student::create(['number' => -1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
     }
 
+
     /** @test */
-    public function it_throws_exception_when_number_already_exists()
+    public function it_throws_exception_when_matriculation_number_already_exists()
     {
-        Student::create(['number' => 1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
-        
-        $this->expectException(\Exception::class);
-        Student::create(['number' => 1, 'first_name' => 'SpongeBob', 'last_name' => 'SquarePants']);
+        Student::create(['name' => 'SpongeBob SquarePants', 'matriculation_number' => '1']);
+
+        $this->expectException(\Illuminate\Database\QueryException::class);
+        Student::create(['name' => 'Another Student', 'matriculation_number' => '1']);
     }
 }
